05a2381ba63b31797a919b691cc066ca0ff49efa
Charu Bansal <bansalc981@gmail.com>
2022-10-04 14:36:40 +0530

    update

--- myapp/views.py
+++ myapp/views.py
@@ -12,7 +12,6 @@ from .models import UserProfile, RegularUpdate, Transaction, Withdraw, CompanyCa
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.utils import timezone
-import pyrebase
 from django.urls import reverse_lazy
 from django.contrib.auth.views import PasswordResetView
 from django.contrib.messages.views import SuccessMessageMixin
